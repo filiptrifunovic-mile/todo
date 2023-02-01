a86d60154312110a02ce26f24e17a2bf54fb1ed2
unknown <filip.trifunovic23@gmail.com>
2023-02-01 14:57:42 +0100

    building layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { AiOutlinePlus } from "react-icons/ai";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+};
+
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo app</h3>
+        <form className={style.form}>
+          <input type="text" placeholder="Add todo" className={style.input} />
+          <button className={style.button}>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul></ul>
+      </div>
     </div>
   );
 }
